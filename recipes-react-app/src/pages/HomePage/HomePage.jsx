195de2b87c00294a46604031fa5d32bc8907c35c
fefusphera <fefu.sphera@hotmail.com>
2022-09-12 16:42:40 +0200

    chore: Navbar tests

--- recipes-react-app/src/pages/HomePage/HomePage.jsx
+++ recipes-react-app/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,10 @@
 import styles from "./index.module.scss";
+// import Navbar from "../Navbar";
 
 const HomePage = () => {
   return (
     <div className={styles.HomePage}>
+      {/* <Navbar /> */}
       <h1 className={styles.Title}>Home</h1>
     </div>
   );
