6ffd11b6f51028a94b0ac258a18e0c4ed4cc0151
fefusphera <fefu.sphera@hotmail.com>
2022-09-13 18:57:48 +0200

    chore: error page css

--- recipes-react-app/src/pages/HomePage/HomePage.jsx
+++ recipes-react-app/src/pages/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
     <div className={styles.HomePage}>
       {/* <Navbar /> */}
       {/* <h1 className={styles.Title}>Home</h1> */}
-      <p className={styles.Paragraph}>Visit our catalogue to get started</p>
+      <p className={styles.Paragraph}>Browse our catalogue to get started</p>
       <img
         className={styles.HomePageImg}
         src="https://t4.ftcdn.net/jpg/00/39/88/01/360_F_39880142_LKhikUlSj863r3V1O5dCVJgrQrt1PTHa.jpg"
